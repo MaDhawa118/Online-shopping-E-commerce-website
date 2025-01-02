0618c99732a792699999ab456782e77be084fdd5
Madhawa118 <dilanmadawa432@gmail.com>
2025-01-03 00:31:25 +0530

    firebase config

--- src/fireabase/FirebaseConfig.jsx
+++ src/fireabase/FirebaseConfig.jsx
@@ -9,15 +9,10 @@ const firebaseConfig = {
   authDomain: "chandra-text-ecommerce.firebaseapp.com",
   projectId: "chandra-text-ecommerce",
   storageBucket: "chandra-text-ecommerce.appspot.com",
-  messagingSenderId: "150191786853",
-  appId: "1:150191786853:web:c497df4f9d93bc4f13af8c"
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase with the provided configuration
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-// Initialize Firestore and Authentication services using the Firebase app instance
-const fireDB = getFirestore(app);// Firestore database instance
-const auth = getAuth(app);// Authentication instance
-// Export Firestore database and Authentication instances for use in other parts of the application
-export {fireDB, auth}
